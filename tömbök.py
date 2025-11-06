4e660d94e860feb83fed9b2d124df665f63d4edd
Pécsi Dániel <pecsi.daniel@students.jedlik.eu>
2025-11-06 15:55:56 +0100

    range, szűrés, módosítások

--- tömbök.py
+++ tömbök.py
@@ -21,3 +21,33 @@ utolsó = számok.pop()   # 5 3 2
 
 for elem in számok:
     print(elem)
+
+print("-------------")
+
+for index in range(1,10):
+    print(index)
+
+print("-------------")
+
+segéd = range(1,10)
+print(segéd[5])
+
+print("-------------")
+print(type(segéd))
+print(type(list(segéd)))
+print(type(számok))
+
+print("-------------")
+print(számok)
+print(f"Első eleme a tömbnek: {számok[0]}")
+print(f"Második eleme a tömbnek: {számok[1]}")
+print(f"Utolsó eleme a tömbnek: {számok[-1]}")
+print(f"2-3. eleme a tömbnek: {számok[1:]}")
+print(f"2-3. eleme a tömbnek: {számok[1:3]}")
+print(számok)
+számok[1:3] = ["három", "kettő"]
+print(számok)
+számok.insert(2, 1000)
+print(számok)
+print("-------------")
+print(len(számok))
